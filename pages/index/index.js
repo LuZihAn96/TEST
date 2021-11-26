e69fa1d109b33a356a2b7dc0a7793dae7920e618
luzihan <lzh17348514422@163.com>
2021-11-26 10:55:52 +0800

    first commit

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,7 +4,7 @@ const app = getApp()
 
 Page({
   data: {
-    motto: 'Hello World',
+    motto: 'Hello LuZihAn',
     userInfo: {},
     hasUserInfo: false,
     canIUse: wx.canIUse('button.open-type.getUserInfo'),
